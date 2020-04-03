b5be2552ebd5923857b93f86c86c286fd4ef6e3f
serhii226 <myzsoftdev1@gmail.com>
2020-04-04 07:16:09 +1000

    update build setting

---
+++ build/precache-manifest.10926dcb63b4b030bf4adf865e32610c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e91285f20714ec3f56a86b8872ed46c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65868af8aa5bf6d9be00",
+    "url": "/static/css/2.44c78b6d.chunk.css"
+  },
+  {
+    "revision": "f4becd51f9c6c82c05b9",
+    "url": "/static/css/main.f95e3692.chunk.css"
+  },
+  {
+    "revision": "65868af8aa5bf6d9be00",
+    "url": "/static/js/2.f0271b75.chunk.js"
+  },
+  {
+    "revision": "6d65dc0fd413da3aeeb59a6c141286d4",
+    "url": "/static/js/2.f0271b75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4becd51f9c6c82c05b9",
+    "url": "/static/js/main.db1c7940.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "7ce2adc984597324c494456004aa097f",
+    "url": "/static/media/ft.7ce2adc9.jpg"
+  },
+  {
+    "revision": "16957b7c79db40f3e8f569965a5cc8b8",
+    "url": "/static/media/hack.16957b7c.jpg"
+  },
+  {
+    "revision": "73fd1e0f9fadf1cde07fb4d8dc307be4",
+    "url": "/static/media/home-bg.73fd1e0f.jpg"
+  }
+]);
