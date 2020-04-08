bbfce73f3c1205903cf4a4d0e844cd6c43f6d239
butsoleg <olegbuts09@gmail.com>
2020-04-08 17:46:35 +1000

    d

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,14 @@ import ContactPage from "./components/pages/contact-page";
 import ComponentSamplesPage from "./components/pages/component-samples";
 import Organization from "./containers/organization";
 
+// Add 
+import GlobalLegalPage from "./components/pages/global-legal";
+import ChallengesListPage from "./components/pages/hk-challenges-list";
+import ParticipantsListPage from "./components/pages/hk-participants-list";
+
+
+
+
 // Import authentication related pages
 import Register from "./containers/auth/register";
 import RegisterOrg from "./containers/auth/register_org";
@@ -16,11 +24,10 @@ import Login from "./containers/auth/login";
 import Logout from "./containers/auth/logout";
 import ForgotPassword from "./containers/auth/forgot_password";
 import ResetPassword from "./containers/auth/reset_password";
-import QRRegister from "./containers/auth/qr_register";
 
 // Import dashboard pages
 import Dashboard from "./containers/dashboard";
-import Profile from "./containers/profile";
+import ViewProfile from "./components/dashboard/profile/view-profile";
 import Inbox from "./components/dashboard/messaging/inbox";
 import Conversation from "./components/dashboard/messaging/conversation";
 import ComposeMessage from "./components/dashboard/messaging/compose-message";
@@ -39,19 +46,24 @@ const Routes = () => (
   <div className="layout">
     <Switch>
       <Route exact path="/" component={HomePage} />
-      <Route path="contact-us" component={ContactPage} />
+      <Route path="/contact-us" component={ContactPage} />
       <Route
-        path="component-samples"
+        path="componentaaa-samples"
         component={RequireAuth(ComponentSamplesPage)}
       />
+      {/* Add */}
+      <Route path="/Global-Legal" component={GlobalLegalPage}/>
+      <Route path="/Challenges-list" component={ChallengesListPage}/>
+      <Route path="/Participants-list" component={ParticipantsListPage}/>
+
+
       <Route path="/register" component={Register} />
       <Route path="/register-org" component={RegisterOrg} />
       <Route path="/login" component={Login} />
       <Route path="/logout" component={Logout} />
       <Route path="/forgot-password" component={ForgotPassword} />
       <Route path="/reset-password/:resetToken" component={ResetPassword} />
-      <Route path="/QRregister" component={QRRegister} />
-      
+
       <Route path="/organization/:id" component={Organization} />
 
       <Route path="/checkout/:plan" component={RequireAuth(InitialCheckout)} />
@@ -59,7 +71,7 @@ const Routes = () => (
         path="/billing/settings"
         component={RequireAuth(BillingSettings)}
       />
-      <Route path="/profile" component={RequireAuth(Profile)} />
+      <Route path="/profile" component={RequireAuth(ViewProfile)} />
 
       <Route path="/admin" component={RequireAuth(AdminDashboard)} />
       <Route exact path="/dashboard" component={RequireAuth(Dashboard)} />
@@ -73,6 +85,7 @@ const Routes = () => (
         component={RequireAuth(Conversation)}
       />
       <Route path="*" component={NotFoundPage} />
+      
     </Switch>
   </div>
 );
